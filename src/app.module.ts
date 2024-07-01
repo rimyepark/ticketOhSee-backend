90b2542c408020958f1bc40dfd1683017e7360af
yerim <sola4718@gmail.com>
2024-07-01 14:12:01 +0900

    add: 데이터 베이스 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicketModule } from './ticket/ticket.module';
+import { TypeOrmConfigService } from "./config/typeorm.config.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
 
 @Module({
-  imports: [TicketModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // 일단 이것은 무조건 가장 위에서!
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // 주목
+      useClass: TypeOrmConfigService,
+      inject: [ConfigService], // 주목
+    }),
+    TicketModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
